9d22113717ea773721c1d60fd05db574f465ac40
ashtrobe <130390213+ashtrobe@users.noreply.github.com>
2024-11-10 13:44:50 +0600

    Update grass.py

--- grass.py
+++ grass.py
@@ -4,29 +4,34 @@ import ssl
 import json
 import time
 import uuid
-import websockets
+import requests
+import shutil
 from loguru import logger
+from websockets_proxy import Proxy, proxy_connect
 from fake_useragent import UserAgent
 
-async def connect_to_wss(user_id):
+async def connect_to_wss(socks5_proxy, user_id):
     user_agent = UserAgent(os=['windows', 'macos', 'linux'], browsers='chrome')
     random_user_agent = user_agent.random
-    device_id = str(uuid.uuid4())
+    device_id = str(uuid.uuid3(uuid.NAMESPACE_DNS, socks5_proxy))
     logger.info(device_id)
     while True:
         try:
             await asyncio.sleep(random.randint(1, 10) / 10)
             custom_headers = {
                 "User-Agent": random_user_agent,
+                "Origin": "chrome-extension://lkbnfiajjmbhnfledhphioinpickokdi"
             }
             ssl_context = ssl.create_default_context()
             ssl_context.check_hostname = False
             ssl_context.verify_mode = ssl.CERT_NONE
             urilist = ["wss://proxy.wynd.network:4444/","wss://proxy.wynd.network:4650/"]
             uri = random.choice(urilist)
+            #uri = "wss://proxy.wynd.network:4650/"
             server_hostname = "proxy.wynd.network"
-            async with websockets.connect(uri, ssl=ssl_context, extra_headers=custom_headers,
-                                          server_hostname=server_hostname) as websocket:
+            proxy = Proxy.from_url(socks5_proxy)
+            async with proxy_connect(uri, proxy=proxy, ssl=ssl_context, server_hostname=server_hostname,
+                                     extra_headers=custom_headers) as websocket:
                 async def send_ping():
                     while True:
                         send_message = json.dumps(
@@ -51,8 +56,9 @@ async def connect_to_wss(user_id):
                                 "user_id": user_id,
                                 "user_agent": custom_headers['User-Agent'],
                                 "timestamp": int(time.time()),
-                                "device_type": "desktop",
-                                "version": "4.28.2",
+                                "device_type": "extension",
+                                "version": "4.26.2",
+                                "extension_id": "lkbnfiajjmbhnfledhphioinpickokdi"
                             }
                         }
                         logger.debug(auth_response)
@@ -64,12 +70,17 @@ async def connect_to_wss(user_id):
                         await websocket.send(json.dumps(pong_response))
         except Exception as e:
             logger.error(e)
+            logger.error(socks5_proxy)
 
 
 async def main():
     #find user_id on the site in conlose localStorage.getItem('userId') (if you can't get it, write allow pasting)
     _user_id = input('Please Enter your user ID: ')
-    await connect_to_wss(_user_id)
+    with open('local_proxies.txt', 'r') as file:
+            local_proxies = file.read().splitlines()
+    tasks = [asyncio.ensure_future(connect_to_wss(i, _user_id)) for i in local_proxies]
+    await asyncio.gather(*tasks)
 
 if __name__ == '__main__':
+    #letsgo
     asyncio.run(main())
